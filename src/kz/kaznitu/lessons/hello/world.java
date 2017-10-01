0319da18e142ddaedcce31378f54ee606ef0c649
Gulaiym Baigalieva <gulaiym.baigalieva@gmail.com>
2017-10-01 19:59:19 +0600

    ekinwi commit

--- src/kz/kaznitu/lessons/hello/world.java
+++ src/kz/kaznitu/lessons/hello/world.java
@@ -3,6 +3,8 @@ package kz.kaznitu.lessons.hello;
 public class world {
     public static void main (String [] args){
         System.out.println("Hello world");
+        System.out.println("зертханалк жумыстын максаты");
+        System.out.println("итпен жумыс истеп уйрену");
 
     }
 }
